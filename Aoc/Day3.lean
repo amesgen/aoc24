a4540abb3b74cc9fb6c515319597159f52f77432
amesgen <amesgen@amesgen.de>
2024-12-03 21:09:15 +0100

    Use parser combinators

--- Aoc/Day3.lean
+++ Aoc/Day3.lean
@@ -1,26 +1,40 @@
 import Aoc.Common
+import Parser
 
 namespace Day3
 
 open Batteries
+open Parser Char ASCII
 
 inductive Instr where
   | Mul (a b : ℕ)
-  | Do | Dont
+  | Do (enabled : Bool)
 deriving Repr
 
+abbrev P := Parser Unit Substring Char
+
 def parse (input : String) : List Instr :=
-  input.trim.toList.tails.filterMap fun l ↦ do
-    let l := l.asString
-    match l.dropPrefix? "mul(" with
-    | none =>
-      if l.startsWith "do()" then some .Do else
-      if l.startsWith "don't()" then some .Dont
-      else none
-    | some l => do
-      let l :: _ :: _ := l.splitOn ")" | none
-      let [a, b] := l.splitOn "," | none
-      pure <| .Mul (← a.toNat?) (← b.toNat?)
+  match Parser.run (p <* endOfInput) input with
+  | .ok _ r => r
+  | .error _ _ => []
+  where
+    p : P (List Instr) := do
+      let instrs ← takeMany (dropUntil pInstr anyToken)
+      dropMany anyToken
+      pure instrs.toList
+
+    pInstr : P Instr := first [pMul, pDo, pDont]
+
+    pMul : P Instr := do
+      let _ ← string "mul("
+      let a ← parseNat
+      let _ ← char ','
+      let b ← parseNat
+      let _ ← char ')'
+      pure <| .Mul a b
+
+    pDo : P Instr := pure (.Do true) <* string "do()"
+    pDont : P Instr := pure (.Do false) <* string "don't()"
 
 def run (input : String) : ℕ × ℕ :=
   let input := parse input
@@ -29,8 +43,7 @@ def run (input : String) : ℕ × ℕ :=
     let rec go (enabled : Bool) : List Instr → ℕ
     | [] => 0
     | .Mul a b :: xs => (if enabled then a * b else 0) + go enabled xs
-    | .Do :: xs => go true xs
-    | .Dont :: xs => go false xs
+    | .Do enabled :: xs => go enabled xs
     go true input
   (part1, part2)
 
