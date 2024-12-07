b971086042f372fe21efa9d0a4ffafcba8ee2d65
amesgen <amesgen@amesgen.de>
2024-12-07 12:27:59 +0100

    Speed up Day 6
    still not exactly fast...

--- Aoc/Day6.lean
+++ Aoc/Day6.lean
@@ -7,6 +7,12 @@ open Batteries
 def parse (input : String) : List (List Char) :=
   input.trim.splitOn "\n" |>.map (·.toList)
 
+abbrev PosDir := (ℤ × ℤ) × (ℤ × ℤ)
+
+structure WalkState where
+  posdir : PosDir
+  visited : Batteries.RBSet PosDir compare
+
 partial def run (input : String) : ℕ × ℕ :=
   let input := parse input
   let grid : RBMap (ℤ × ℤ) Char :=
@@ -15,34 +21,37 @@ partial def run (input : String) : ℕ × ℕ :=
   let initialGuard := grid.toList
     |>.find? (fun (_, c) ↦ c == '^') |>.map (·.1) |>.getD (0, 0)
   let initialDir : ℤ × ℤ := (0, -1)
+  let initialState : WalkState := {
+     posdir := (initialGuard, initialDir),
+     visited := ∅
+  }
 
-  let step (grid : RBMap (ℤ × ℤ) Char) (pos dir : ℤ × ℤ)
-    : Option ((ℤ × ℤ) × (ℤ × ℤ)) :=
+  let step (grid : RBMap (ℤ × ℤ) Char) (posdir : PosDir) : Option PosDir :=
+    let (pos, dir) := posdir
     let  pos' := pos + dir
     grid.find? pos' |>.map fun
-      | '#' =>
-        let dir' := (- dir.2, dir.1)
-        (pos, dir')
+      | '#' => (pos, (- dir.2, dir.1))
       | _ => (pos', dir)
+  let rec walk (grid : RBMap (ℤ × ℤ) Char)
+          (s : WalkState) : Option (List WalkState) :=
+    if s.visited.contains s.posdir then none else
+      let visited := s.visited.insert s.posdir
+      match step grid s.posdir with
+      | some posdir => (s :: ·) <$> walk grid { posdir, visited }
+      | none => [s, { s with visited }]
+  let walks := walk grid initialState |>.getD []
 
-  let visited (grid : RBMap (ℤ × ℤ) Char) : Option (RBSet (ℤ × ℤ)) :=
-    let rec go (visited : RBSet ((ℤ × ℤ) × (ℤ × ℤ))) (pos dir : ℤ × ℤ)
-      : Option (RBSet ((ℤ × ℤ) × (ℤ × ℤ))) :=
-      if visited.contains (pos, dir) then none else
-        let visited := visited.insert (pos, dir)
-        match step grid pos dir with
-        | some (pos', dir') => go visited pos' dir'
-        | none => visited
-    go ∅ initialGuard initialDir |>.map (·.map (·.1))
+  let part1 := RBSet.ofList (walks.map (·.posdir.1)) compare |>.size
 
-  let part1 := visited grid |>.map (·.size) |>.getD 0
-  let part2 := grid.toList
-    -- prune candidates more efficiently
-    |>.filterMap (fun (p, c) ↦ do
-       guard <| c == '.'
-       guard <| visited (grid.insert p '#') |>.isNone
-       pure p)
-    |>.length
+  let dirs : List (ℤ × ℤ) := [(1, 0), (0, 1), (-1, 0), (0, -1)]
+  let obstrs := walks.filterMap fun s ↦ do
+    let pos' := s.posdir.1 + s.posdir.2
+    guard <| grid.find? pos' == '.'
+    guard <| ! dirs.any fun d ↦ s.visited.contains (pos', d)
+    guard <| walk (grid.insert pos' '#') s |>.isNone
+    pure pos'
+  let part2 := RBSet.ofList obstrs compare |>.size
+
   (part1, part2)
 
 def ex := "
